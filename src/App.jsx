472d45f863a436d565983d355a1ac2103ec0305f
unknown <kishorekrish69118@gmail.com>
2026-06-08 19:09:46 +0530

    Updated blog portfolio project

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
 import Home from "./components/Home";
 import Blogs from "./components/Blogs";
 import Navbar from "./components/common/Navbar";
-import { BrowserRouter,Route,Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import About from "./components/About";
+
 function App() {
   return (
-   <div className="px-10  bg-white border rounded-md">
     <BrowserRouter>
-    <Navbar/>
-    <Routes>
-      <Route path="/home" element={<Home/>}></Route>
-      <Route path="/blogs" element={<Blogs/>}></Route>
-      <Route path="/login" element={<Login/>}></Route>
-      <Route path="/signup" element={<Signup/>}></Route>
-      <Route path="/about" element={<About />} />
-    </Routes>
+      <div className="px-10 bg-white border rounded-md">
+        <Navbar />
 
+        <Routes>
+          {/* Default Route */}
+          <Route path="/" element={<Navigate to="/home" />} />
+
+          {/* Pages */}
+          <Route path="/home" element={<Home />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/about" element={<About />} />
+
+          {/* 404 Page */}
+          <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+        </Routes>
+      </div>
     </BrowserRouter>
-   </div>
   );
 }
 
